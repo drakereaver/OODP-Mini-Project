4834897e2f9731644c5537b8cb4e654b66e81339
Kshitij <kshitijmanjhi@gmail.com>
2025-01-22 10:36:02 +0530

    Update Rock_Paper_Scissors_Game.c
    changed break to return 0

--- Rock_Paper_Scissors_Game.c
+++ Rock_Paper_Scissors_Game.c
@@ -9,22 +9,22 @@ int main(){
     int rnum = 0;                                       // random number
     char options[3][10] = {"rock","paper","scissors"};  // 2D array of options
     char pChoice[10] = {'\0'};                          // player choice
-    char cmpChoice[10];                                 // computer choice
+    char cmpChoice[10];                                 
     printf("Welcome To Rock Paper Scissors Game\n");
     printf("type '!score' to view score or type '!stop' to exit:\n");
     while (1) {
         printf("Enter your choice (rock, paper, scissors): \n");
         fgets(pChoice, sizeof(pChoice), stdin);         // read input with newline character
         pChoice[strcspn(pChoice, "\n")] = '\0';         // remove newline character if present
-        if (strcmp(pChoice,"!stop") == 0){              
-            break;                                      
+        if (strcmp(pChoice,"!stop") == 0){             
+            return 0;                                      
         } 
         else if (strcmp(pChoice, "!score") == 0)
         {
             printf("Your score is: %d\n",score);
             continue;
         }
-        else if (!(strcmp(pChoice, "rock") == 0 || strcmp(pChoice, "paper") == 0 || strcmp(pChoice, "scissors") == 0)){ // 0 equal, 1 first string big , -1 second string big
+        else if (!(strcmp(pChoice, "rock") == 0 || strcmp(pChoice, "paper") == 0 || strcmp(pChoice, "scissors") == 0)){
             printf("Invalid input\n");
             continue;
         }
@@ -43,8 +43,8 @@ int main(){
         } else {
             printf("You lose!\n");
             if (score >0 ){
-            score--;
-            }
+            score--;}
+
         }
     }
     return 0;
